51e1e120eb6bd4367e19ea53ea7a4a12b2e4ed84
Farhaan mahbub <farhan.meb@gmail.com>
2022-01-31 14:55:51 +0600

    Update axiosInstance.js

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,8 +1,7 @@
 import Axios from "axios";
 
 const axiosInstance = Axios.create({
-    // baseURL: "http://localhost:4000/",
-    baseURL: "http://192.168.68.142:4000",
+    baseURL: "http://localhost:4000/",
     withCredentials: true,
     "Content-Type": "application/json",
 });
