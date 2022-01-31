a0229030251ca28593b91f52ed17caea3e51878b
Farhaan mahbub <farhan.meb@gmail.com>
2022-01-31 11:35:29 +0600

    Import bulk employee completed. Task Done!

--- src/components/Employee/AddBulkEmployee/AddBulkEmployee.jsx
+++ src/components/Employee/AddBulkEmployee/AddBulkEmployee.jsx
@@ -3,16 +3,22 @@ import axiosInstance from "../../../services/axiosInstance";
 import Loader from "../../Loader/Loader";
 import "./AddBulkEmployee.scss";
 
-const AddBulkEmployee = ({ loader }) => {
+const AddBulkEmployee = ({
+    fileValue,
+    loader,
+    handleFileChange,
+    handleAddBulkEmployee,
+}) => {
     return (
         <div className="import-product-section">
-            <form className="import-form">
+            <form className="import-form" onSubmit={handleAddBulkEmployee}>
                 <div className="import-product-body">
                     <input
                         className="input-form"
-                        // onChange={handleFileChange}
+                        onChange={handleFileChange}
                         type="file"
                         name="file"
+                        value={fileValue}
                         accept="*.csv"
                         required
                     />
--- src/pages/AddEmployee/AddEmployee.jsx
+++ src/pages/AddEmployee/AddEmployee.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import axiosInstance from "../../services/axiosInstance";
 import Helmet from "react-helmet";
@@ -12,7 +12,9 @@ const AddEmployee = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
+    const [file, setFile] = useState(null);
     const [loader, setLoader] = useState(false);
+    const [fileValue, setFileValue] = useState("");
 
     const handleAddEmployee = (e) => {
         e.preventDefault();
@@ -38,14 +40,43 @@ const AddEmployee = () => {
             });
     };
 
+    const handleAddBulkEmployee = (e) => {
+        e.preventDefault();
+        setLoader(true);
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        axiosInstance
+            .post("api/employee/import-bulk-employees", formData)
+            .then((res) => {
+                toast.success(res.data.succeeded);
+                toast.error(res.data.failed);
+                setFileValue("");
+            })
+            .finally(() => {
+                setLoader(false);
+            });
+    };
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+        setFileValue(e.target.value);
+    };
+
     return (
         <>
             <div className="add-employee-container">
                 <Helmet>
                     <title>Add Employee</title>
                 </Helmet>
+                <h4 className="header-text">HR-Management / Add Employee</h4>
                 <div className="add-employee">
-                    <AddBulkEmployee />
+                    <AddBulkEmployee
+                        fileValue={fileValue}
+                        handleFileChange={handleFileChange}
+                        handleAddBulkEmployee={handleAddBulkEmployee}
+                    />
                     <div className="form">
                         <AddEmployeeForm
                             firstName={firstName}
